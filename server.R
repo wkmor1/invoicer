f5d1ac729ff1a5e96df677023ad1d31884cfda10
wkmor1 <wkmor1@gmail.com>
2016-05-08 19:10:30 +1000

    source .bash_profile in server

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ shinyServer(
       downloadHandler(
         "invoice.pdf",
         function(file) {
+          system(". ~/.bash_profile")
           rmarkdown::render("invoice.Rmd")
           file.copy("invoice.pdf", file)
         }
